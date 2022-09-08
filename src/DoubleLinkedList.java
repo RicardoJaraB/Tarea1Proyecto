853d20270815b0c93ad10aa05c46775cbcfa336a
Asthira <sketchjarablanco@gmail.com>
2022-09-07 21:47:00 -0300

    actualizado los metodos

--- src/DoubleLinkedList.java
+++ src/DoubleLinkedList.java
@@ -1,13 +1,51 @@
 
 public class DoubleLinkedList {
-	String nombre;
-	DoubleLinkedList List;
+	private DoubleLinkedListItem first;
+	private DoubleLinkedListItem last;
 	
-	public DoubleLinkedList(String nombre) {
+	public DoubleLinkedList() {
 		super();
-		this.nombre = nombre;
-
+		this.first = null;
+		this.last = null;
 	}
 	
+	public void AddFirst(DoubleLinkedListItem item) {
+		if (this.first==null) {
+			this.first=item;
+			this.last=item;
+			
+			item.setNext(item);
+			item.setPreciusly(item);
+		}
+		
+		else {
+			item.setNext(first);
+			item.setPreciusly(last);
+			last.setNext(item);
+			first.setPreciusly(item);
+			this.first=item;
+		}
+	}
+	
+	public void AddLast(DoubleLinkedListItem item) {
+		if (this.first==null) {
+			this.first=item;
+			this.last=item;
+			
+			item.setNext(item);
+			item.setPreciusly(item);
+		}
+		
+		else {
+			item.setNext(first);
+			item.setPreciusly(last);
+			last.setNext(item);
+			first.setPreciusly(item);
+			this.last=item;
+		}		
+		
+		
+		
+	}
 
 }
--- src/DoubleLinkedListItem.java
+++ src/DoubleLinkedListItem.java
@@ -1,4 +1,39 @@
 
 public class DoubleLinkedListItem {
+	private DoubleLinkedList nombre;
+	private DoubleLinkedListItem next;
+	private DoubleLinkedListItem previusly;
+	
+	
+	public DoubleLinkedListItem() {
+		super();
+		this.nombre=null;
+		this.next = null;
+		this.previusly = null;
+	}
 
+	public void setNext (DoubleLinkedListItem next) {
+		this.next = next;
+	}
+	
+	public void setPreciusly (DoubleLinkedListItem previusly) {
+		this.previusly = previusly;
+	}
+	
+	public DoubleLinkedListItem getNext (DoubleLinkedListItem next) {
+		return this.next;
+	}
+	
+	public DoubleLinkedListItem getPreciusly (DoubleLinkedListItem previusly) {
+		return this.previusly;
+	}
+	
+	public DoubleLinkedList getList() {
+		return this.nombre;
+	}
+	
+	public void setList (DoubleLinkedList nombre) {
+		this.nombre = nombre;
+	}
+	
 }
